5c5ce7ccef1ca322b29735018a7336f63711abdf
Ritiksingh70045 <ritiksingh70045@gmail.com>
2026-03-11 23:33:11 +0530

    Some bugs

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,32 +1,28 @@
-//Middleware to check used Id and has PremiumPlan
-
 import { clerkClient } from "@clerk/express";
 
-export const auth = async(req, res, next) => {
-  try{
-    const {userId , has} = await req.auth();
-    const hasPremiumPlan = has({plan : 'premium'});
+// Middleware to check userId and hasPremiumPlan
+
+export const auth = async (req, res, next) => {
+  try {
+    const { userId, has } = await req.auth();
+    const hasPremiumPlan = await has({ plan: "premium" });
 
     const user = await clerkClient.users.getUser(userId);
 
-    if(!hasPremiumPlan && user.privateMetadata.free_usage)
-    {
+    if (!hasPremiumPlan && user.privateMetadata.free_usage) {
       req.free_usage = user.privateMetadata.free_usage;
-    }
-    else
-    {
-      await clerkClient.users.updateUserMetadata(userId , {
-        privateMetadata : {
-          free_usage : 0
-        }
-      })
+    } else {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: 0,
+        },
+      });
       req.free_usage = 0;
     }
 
-    req.plan = hasPremiumPlan ? 'premium' : 'free';
+    req.plan = hasPremiumPlan ? "premium" : "free";
     next();
+  } catch (error) {
+    res.json({ success: false, message: "Error in Auth Middleware" });
   }
-  catch(error){
-    res.json({success : false , message : "Error in auth middleware"});
-  }
-}
+};
